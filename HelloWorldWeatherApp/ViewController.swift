54e8dfad79699d23e5ceb78d7d385b7310012b2e
admin <vasilyevanton_92@gmail.com>
2018-01-29 08:48:14 +0200

    Initial Commit

---
+++ HelloWorldWeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorldWeatherApp
+//
+//  Created by admin on 29.01.2018.
+//  Copyright © 2018 home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
